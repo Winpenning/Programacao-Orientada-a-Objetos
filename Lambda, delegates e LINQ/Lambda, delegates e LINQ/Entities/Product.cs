0a0e4947c642c2fe377de46c373512c1e676856c
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-15 15:18:12 -0300

    Solução Ruim utilizando o IComparable.

---
+++ Lambda, delegates e LINQ/Lambda, delegates e LINQ/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Lambda__delegates_e_LINQ.Entities
+{
+    public class Product : IComparable<Product>
+    {
+        private string name;
+        private double price;
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public int CompareTo(Product other)
+        {
+            return name.ToUpper().CompareTo(other.name.ToUpper());
+        }
+        public override string ToString()
+        {
+            return name + " " + price;
+        }
+    }
+}
---
+++ Lambda, delegates e LINQ/Lambda, delegates e LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lambda__delegates_e_LINQ.Entities;
+using System.Globalization;
+
+namespace Lambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("Fogão Industrial", 6000));
+            products.Add(new Product("Xbox Series X", 3500));
+            products.Add(new Product("Alicate", 99.99));
+            products.Sort();
+            
+            
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        } 
+    }
+}

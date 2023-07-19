3d358997f61c8f7f4436e52c7d1dfa7fe1fdc3aa
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-19 18:08:42 -0300

    Predicate

---
+++ Lambda, delegates e LINQ/delegates/Delegates/Delegates/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Entities
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public override string ToString()
+        {
+            return name + " " + price;
+        }
+    }
+}
--- Lambda, delegates e LINQ/delegates/Delegates/Delegates/Program.cs
+++ Lambda, delegates e LINQ/delegates/Delegates/Delegates/Program.cs
@@ -1,4 +1,5 @@
-﻿using Delegates.Services;
+﻿using Delegates.Entities;
+using Delegates.Services;
 using System;
 
 namespace Delegates
@@ -8,12 +9,45 @@ namespace Delegates
     {
         public static void Main(string[] args)
         {
+            Predicates();
+        }
+        public static void MulticastDelegates()
+        {
             double a = 10;
             double b = 12;
             BinaryNumericOperation op = CalculationService.ShowSum;
             op += CalculationService.ShowMax;
 
-            op.Invoke(a,b);
+            op.Invoke(a, b);
+        }
+        public static void Predicates()
+        {
+            /*Fazer um programa que, a partir de uma lista de produtos, remova da
+            lista somente aqueles cujo preço mínimo seja 100.*/
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+    
+            // UTILIZANDO lambda
+            //list.RemoveAll(p => p.price >= 100.00);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("\n");
+
+            list.RemoveAll(Predicates2);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public static bool Predicates2(Product p)
+        {
+            return p.price >= 100;
         }
     }
 }

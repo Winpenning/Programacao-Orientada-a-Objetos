77057ccded9816c97965779c0477ad38adaedfc7
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-20 19:27:12 -0300

    Delegate Func<> para representar métodos que recebem 0 ou mais argumentos e retornam um tipo genérico.

--- Lambda, delegates e LINQ/delegates/Delegates/Delegates/Program.cs
+++ Lambda, delegates e LINQ/delegates/Delegates/Delegates/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Delegates.Entities;
 using Delegates.Services;
 using System;
+using System.Linq;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
 
 namespace Delegates
 {
@@ -10,10 +13,11 @@ namespace Delegates
     {
         public static void Main(string[] args)
         {
-            Action();
+            Func();
+            //  Action();
             //ExamplePredicates(new Employee(1,"Paulo",500,700));
-          //  MulticastDelegates();
-         //   Predicates();
+            //  MulticastDelegates();
+            //   Predicates();
         }
         public static void MulticastDelegates()
         {
@@ -73,14 +77,15 @@ namespace Delegates
             list.Add(new Product("Xbox Series X", 6000));
 
             // podemos fazer assim
-            Action<Product> aumento = p => p.price += p.price * 0.1;
+            Action<Product> aumento = p => { p.price += p.price * 0.1; };
+            // coloca-se chaves na lambda quando damos um comando. Sem retorno.
             list.ForEach(aumento);
 
             foreach(Product p in list) 
             {
                 Console.WriteLine(p);
             }
-
+            
             // ou assim
             list.ForEach(p => p.price += p.price * 0.1);
             foreach (Product p in list)
@@ -99,6 +104,51 @@ namespace Delegates
         public static void Action2(Product p)
         {
             p.price += p.price * 0.1;
+        }
+        public static void Func()
+        {
+            /*Fazer um programa que, a partir de uma lista de produtos, 
+            gere uma nova lista contendo os nomes dos produtos em caixa alta.*/
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+    
+            // podemos fazer assim
+            List<string> list2 =  list.Select<Product, string>(Func2).ToList();
+
+            foreach(string p in list2)
+            {
+                Console.WriteLine(p);
+            }
+
+            // ou assim
+            Func<Product, string> func = Func2;
+            List<string> list3 = list.Select<Product, string>(func).ToList();
+            foreach (string p in list3)
+            {
+                Console.WriteLine(p);
+            }
+
+            // ou assim
+            Func<Product, string> func2 = p => p.name.ToUpper();
+            List<string> list4 = list.Select(func2).ToList();
+            foreach (string p in list4)
+            {
+                Console.WriteLine(p);
+            }
+
+            // ou ainda podemos fazer inline
+            List<string> list5 = list.Select(p => p.name.ToUpper()).ToList();
+            foreach (string p in list5)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public static string Func2(Product p)
+        {
+            return p.name.ToUpper();
         }
         
     }

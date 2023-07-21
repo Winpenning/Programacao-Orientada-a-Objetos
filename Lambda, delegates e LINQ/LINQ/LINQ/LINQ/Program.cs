a1d1073f388d3b5286f86c83899c37e300156fc0
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-21 14:22:16 -0300

    Introdução ao LINQ

---
+++ Lambda, delegates e LINQ/LINQ/LINQ/LINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+namespace LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            IEnumerable<int> enumerable = 
+                list.Where(x => x % 2 == 0).Select(x => x * 10); ;
+
+            foreach (int x in enumerable)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

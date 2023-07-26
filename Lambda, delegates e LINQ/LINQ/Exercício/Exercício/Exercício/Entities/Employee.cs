5b2cdfb299e2a5d27fd841b38622352e846286f1
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-26 15:46:19 -0300

    exercício concluído

---
+++ Lambda, delegates e LINQ/LINQ/Exercício/Exercício/Exercício/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício.Entities
+{
+    internal class Employee
+    {
+        private string Name { get; set; }
+        public string Email { get; set; }
+        private double Salary { get; set; }
+        public Employee(string Name, string Email, double Salary) {
+            this.Name = Name;
+            this.Email = Email;
+            this.Salary = Salary;
+        }
+
+        public double getSalary()
+        {
+            return Salary;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public string getEmail()
+        {
+            return Email;
+        }
+    }
+}
--- Lambda, delegates e LINQ/LINQ/Exercício/Exercício/Exercício/Program.cs
+++ Lambda, delegates e LINQ/LINQ/Exercício/Exercício/Exercício/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Exercício
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Exercício.Entities;
+using System.IO;
+using System.Linq;
+namespace Exercício
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the file path: ");
+            string path = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            double value = double.Parse(Console.ReadLine());
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream) 
+                {
+                    string[] field = sr.ReadLine().Split(',');
+                    string name = field[0];
+                    string email = field[1];
+                    double salary = Convert.ToDouble(field[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+
+
+            var emails = 
+                list.Where(p => p.getSalary() > value && p.getEmail != null)
+                .OrderBy(p => p.Email)
+                .Select(p => p.getEmail());
+
+            Console.WriteLine("Email dos funcionários com salário superior a " + value);
+            foreach (string nam in emails)
+            {
+                Console.WriteLine(nam);
+            }
+
+            double Msum = list.Where(p => p.getName()[0] == 'M')
+                .Select(p => p.getSalary()).Sum();
+
+            Console.WriteLine("Soma dos salários das pessoas que começam com a letra 'M' "+Msum);
+
+
         }
     }
 }

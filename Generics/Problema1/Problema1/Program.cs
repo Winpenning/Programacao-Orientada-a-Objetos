e315912e31b3041344aa2465e26929c88f59e5bf
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-31 13:59:56 -0300

    Problema motivador 1, serviços e reuso, expressões de nível superior

---
+++ Generics/Problema1/Problema1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Problema1.Services;
+
+Console.Write("How many values? ");
+int values = int.Parse(Console.ReadLine());
+
+PrintService printService = new PrintService();
+
+for(int i = 1; i <= values; i++)
+{
+    int value = int.Parse(Console.ReadLine());
+    printService.AddValue(value);
+}
+
+printService.Print();
+
+Console.WriteLine("\nFirst: " + printService.First());
---
+++ Generics/Problema1/Problema1/Services/PrintService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1.Services
+{
+    public class PrintService
+    {
+        private int[] Array = new int[10];
+        private int _Count = 0;
+        public void AddValue(int value)
+        {
+            if (_Count == 10)
+            {
+                throw new InvalidOperationException("EXCEPTION! PrintService is full!");
+            }
+            else
+            {
+                Array[_Count] = value;
+                _Count++;
+            }
+        }
+        public int First()
+        {
+            if (_Count == 0)
+            {
+                throw new IndexOutOfRangeException("EXCEPTION! PrintService is empty!");
+            }
+            else
+            {
+                return Array[0];
+            }
+        }
+        public void Print()
+        {
+            if (_Count == 0)
+            {
+                throw new IndexOutOfRangeException("EXCEPTION! PrintService is empty!");
+            }
+            else
+            {
+                Console.Write("[");
+                for (int i = 0; i < _Count - 1; i++)
+                {
+                    Console.Write(Array[i] + ", ");
+                }
+                Console.Write(Array[_Count - 1]);
+                Console.Write("]");
+            }
+        }
+    }
+}

fe23d134264d6ace836235bf7494c72bd367d27d
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-08-07 15:44:14 -0300

    HashSet e SortedSet

---
+++ HashSet e SortedSet/HashSet e SortedSet/Entities/People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet_e_SortedSet.Entities
+{
+    public class People
+    {
+        public Int64 Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public People(Int64 cpf, string nome, string email)
+        {
+            this.Cpf = cpf;
+            this.Nome = nome;
+            this.Email = email;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + 
+                   "\nCpf: " + Cpf +
+                   "\nE-mail: " + Email;
+        }
+    }
+}
---
+++ HashSet e SortedSet/HashSet e SortedSet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using HashSet_e_SortedSet.Entities;
+
+
+/*HashSet<People> peoples = new HashSet<People>();
+peoples.Add(new People(09909999990, "Paulo Henrique", "paulo@gmail.com"));
+peoples.Add(new People(099099999950, "Maria Antonieta", "maria@gmail.com"));
+peoples.Add(new People(09909999950, "Sun Tzu", "sun@gmail.com"));*/
+
+HashSet<string> setviahash = new HashSet<string>();
+
+setviahash.Add("Antônio");
+setviahash.Add("Clara");
+setviahash.Add("Lúcia");
+setviahash.Add("Rosevelt");
+
+Console.WriteLine(setviahash.Contains("Antônio"));
+
+foreach (string item in setviahash)
+{
+    Console.WriteLine(item);
+}
